fabb8bee98ce1d335c9ed1d4750d74799ab9fcae
Seba Collado <scollado@ujaen.es>
2019-06-27 21:54:47 +0200

    hotfix

--- html/application/views/home.php
+++ html/application/views/home.php
@@ -9,7 +9,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-<?php echo $_SERVER["HTTP_HOST"]; ?>
 	<div class="container mt-5">
 		<div class="row">
 			<div class="col-12">
